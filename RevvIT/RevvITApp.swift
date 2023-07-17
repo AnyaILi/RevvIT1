cc26557d8a9e50235c68cc7a38ee2c92fc67d7fe
AnyaILi <anyali0219@gmail.com>
2023-07-17 14:54:10 -0700

    Initial Commit

---
+++ RevvIT/RevvITApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevvITApp.swift
+//  RevvIT
+//
+//  Created by Scholar on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct RevvITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
